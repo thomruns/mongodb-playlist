44cfeef0188ca38c352e50d2c92c5168d73192b2
Thom David <thomdavid74@gmail.com>
2018-03-09 09:12:14 -0800

    Added find test

--- models/mariochar.js
+++ models/mariochar.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose'); // require mongoos
+const mongoose = require('mongoose'); // require mongoose
 const Schema = mongoose.Schema;  // mongoose Schema constructor function
 
 // Create schema and model
--- test/finding_test.js
+++ test/finding_test.js
@@ -4,24 +4,43 @@ const MarioChar = require('../models/mariochar'); //use the model schema file
 // Describe tests
 describe('Finding records', function(){
 
+    var char; 
     beforeEach(function(done){
-        var char = new MarioChar({
+        char = new MarioChar({
             name: 'Mario',
             weight: 60
         });
         char.save().then(function(){
             //assert(char.isNew === false);
             done();
-        });
+        }).catch(function() {
+            console.log("Promise Rejected");
+            done();
+       });
     });
 
     // Create tests -- each it block describes an individual test
-    it('Finds one record from the database', function() {
+    it('Finds one record from the database', function(done) {
       
         MarioChar.findOne({name: 'Mario'}).then(function(result){
             assert(result.name === 'Mario');
             done();
-        });
+        }).catch(function() {
+            console.log("Promise Rejected");
+            done();
+       });
+
+    });
+
+    it('Finds one record by ID from the database', function(done) {
+      
+        MarioChar.findOne({_id: char._id}).then(function(result){
+            assert(result._id.toString() === char._id.toString());
+            done();
+        }).catch(function(err) {
+            console.log("Promise Rejected " + err);
+            done();
+       });
 
     });
 
